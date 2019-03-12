d1a812178848054d1f8795c3a36630ac33750289
Kyaw Naing Win <knwin@users.noreply.github.com>
2019-03-13 02:26:05 +0630

    Update language.py

--- myanmar/language.py
+++ myanmar/language.py
@@ -189,6 +189,10 @@ def PhonemicSyllableBreak(text, encoding):
             syllable = {k: v for k, v in match.groupdict().items() if v}
             start = match.end()
 
-        yield syllable
-
+        #yield syllable
+        try:
+            yield syllable
+        except StopIteration:
+            return
+        
     raise StopIteration
